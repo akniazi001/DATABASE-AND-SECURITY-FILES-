be31b1ed4bef2d48e7c2ce32c54a2c0b91c620bd
Altaf Khan Niazi <akniazi001@users.noreply.github.com>
2021-04-30 10:08:38 +0300

    Add files via upload
    Implementation of Yen Shen Hwang's One Time Password using c#.net
    The report is attached here.

---
+++ One Time Password/OneTimePassword/OneTimePassword/Default.aspx
@@ -0,0 +1,240 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="OneTimePassword.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+        .style2
+        {
+            text-align: left;
+        }
+        .style3
+        {
+            text-align: center;
+            color: #6699FF;
+            font-weight: bold;
+        }
+        .style4
+        {
+            color: #000099;
+        }
+        .style5
+        {
+            text-align: right;
+            width: 315px;
+        }
+        .style6
+        {
+            width: 315px;
+            text-align: right;
+        }
+        .style7
+        {
+            width: 315px;
+            text-align: center;
+            height: 30px;
+        }
+        .style8
+        {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+  <div>
+  <div id= "div_registerdata" runat="server">
+    <div id="div_registerprocess">
+    <table class="style1">
+        <tr>
+            <td class="style3" colspan="2">
+                <h2 class="style4">
+                    One Time Password Implementation
+                </h2>
+            </td>
+        </tr>
+        <tr>
+            <td class="style2" colspan="2">
+                First Register to Server and click below button and get Smart card number. 
+                if already Register
+                <asp:LinkButton ID="LinkButton1" runat="server" onclick="LinkButton1_Click">click here</asp:LinkButton>
+            </td>
+        </tr>
+        <tr>
+            <td class="style5">
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style5">
+                &nbsp;</td>
+            <td>
+                <asp:Button ID="btnregister" runat="server" onclick="btnregister_Click" 
+                    Text="Register Now " Height="30px" Width="146px" />
+            </td>
+        </tr>
+        </table>
+    </div>
+    <div id="div_showseed" runat="server"  visible="false">
+    
+        <table class="style1">
+            <tr>
+                <td class="style6">
+                    Smart Card (Seed): </td>
+                <td>
+                <asp:Label ID="lblSeed" runat="server"></asp:Label>
+            &nbsp; </td>
+            </tr>
+            <tr>
+                <td class="style7">
+                    </td>
+                <td class="style8">
+                    &nbsp;<asp:Button ID="btngetsecrete" runat="server" Text="Go farword " 
+                        onclick="btngetsecrete_Click" />
+                </td>
+            </tr>
+            </table>
+    
+    </div>
+    <div id="id_shownounceandmessage" runat="server" visible="false">
+    
+        <table class="style1">
+            <tr>
+                <td class="style6">
+                    Secrete code(Nounce 
+                    D and N value) : </td>
+                <td>
+                    <asp:Label ID="lblshownounce_fruser" runat="server"></asp:Label>
+                &nbsp;and N:
+                    <asp:Label ID="lbl_clientN" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="lblshow_correctcode" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Button ID="btnlogin" runat="server" Text=" Process farword to authenticate " 
+                        Visible="False" onclick="btnlogin_Click" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+   <div id="div_usrnamePass" runat= "server" visible="false">
+    
+        <table class="style1">
+            <tr>
+                <td class="style6">
+                    Enter Username : </td>
+                <td>
+                    <asp:TextBox ID="txtusername" runat="server" Width="220px" ValidationGroup="login"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server"  ValidationGroup="login"
+                         ControlToValidate="txtusername" ErrorMessage="* Required field cannot be left blank."
+                  Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    Password : </td>
+                <td>
+                    <asp:TextBox ID="txtpassword" runat="server" TextMode="Password" Width="220px" 
+                        CausesValidation="True" ToolTip="password " ValidationGroup="login"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server"  ValidationGroup="login"
+                          ControlToValidate="txtpassword" ErrorMessage="* Required field cannot be left blank."
+                           ForeColor="Red"  Display="Dynamic">
+                        </asp:RequiredFieldValidator>
+
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="lblshow_register_errormsg" runat="server"></asp:Label>
+
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Button ID="btn_savepass" runat="server" Text="Log in" 
+                        onclick="btn_savepass_Click" ValidationGroup="login" />
+                </td>
+            </tr>
+        </table>
+    
+   </div>
+   </div>
+ <div id="div_loginuser" runat="server" visible="false">
+ 
+        <table class="style1">
+            <tr>
+                <td class="style6">
+                    Enter Username : </td>
+                <td>
+                    <asp:TextBox ID="txtlogin_username" runat="server" Width="220px"></asp:TextBox>
+                    
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ValidationGroup="login_auth"
+    ControlToValidate="txtlogin_username" ErrorMessage="* Required field cannot be left blank."
+    Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    Password : </td>
+                <td>
+                    <asp:TextBox ID="txtlogin_password" runat="server" TextMode="Password" 
+                        Width="220px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ValidationGroup="login_auth"
+    ControlToValidate="txtlogin_password" ErrorMessage="* Required field cannot be left blank."
+    Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator></td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    Enter Seed: </td>
+                <td>
+                    <asp:TextBox ID="txtlogin_seed" runat="server" Width="220px"></asp:TextBox>
+                    
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ValidationGroup="login_auth"
+    ControlToValidate="txtlogin_seed" ErrorMessage="* Required field cannot be left blank."
+    Display="Dynamic" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="lbllogin_messageshow" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    &nbsp;</td>
+                <td>
+                    <asp:Button ID="btn_login_auth" runat="server" Text="login User" 
+                        onclick="btn_login_auth_Click" ValidationGroup="login_auth" 
+                       />
+                </td>
+            </tr>
+        </table>
+    
+ </div>
+   </div>
+    </form>
+</body>
+</html>
---
+++ One Time Password/OneTimePassword/OneTimePassword/Default.aspx.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OneTimePassword
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //string seed, nounce, N, hasvalue;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnregister_Click(object sender, EventArgs e)
+        {
+            Label lblnounce = new Label();
+            Label lblXor = new Label();
+            Label lblhashD0 = new Label();
+            lblSeed.Text =Convert.ToString( getSeed()); 
+            lblnounce.Text = getNounce().ToString();
+            Session["seed"] = lblSeed.Text;
+            lblXor.Text = GetXOR(lblnounce.Text,lblSeed.Text);
+            Session["seedXORnounce"] = lblXor.Text;
+            lblhashD0.Text = GetHash( lblnounce.Text,lblSeed.Text);
+            Session["hD"] = lblhashD0.Text;
+            div_showseed.Visible = true;
+        }
+        protected string GetHash(string password, string salt)
+         {
+                 MD5 md5 = new MD5CryptoServiceProvider();
+                 byte [] digest = md5.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
+                 string base64digest = Convert.ToBase64String(digest, 0, digest.Length);
+                 return base64digest.Substring(0, base64digest.Length-2);
+         }
+        protected string GetXOR( string input,string key ) 
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+                sb.Append((char)(input[i] ^ key[(i % key.Length)]));
+            String result = sb.ToString();
+
+            return result;
+        }
+        protected int getSeed() 
+        {
+            Random r = new Random();
+            int x = r.Next(1000, 5000);//Max range
+            return x;        
+        }
+        protected int getNounce()
+        {
+            Random n = new Random();
+            int nounce = n.Next(5001, 9990);//Max range
+            return nounce;
+        }
+        protected int getN()
+        {
+            Random n = new Random();
+            int Num = n.Next(2, 10);//Max range
+            return Num;
+         }
+       
+        protected void btngetsecrete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string lblN;
+                string newhash;
+                lblN = getN().ToString();
+                Session["N"] = lblN;
+                lbl_clientN.Text = lblN;
+                string xornounce = Session["seedXORnounce"].ToString();
+                lblshownounce_fruser.Text = GetXOR(lblSeed.Text, xornounce);
+                newhash = GetHash(lblshownounce_fruser.Text, lblSeed.Text);
+                string oldhash = Session["hD"].ToString();
+                if (newhash.Length == oldhash.Length)
+                {
+                    int i = 0;
+                    while ((i < newhash.Length) && (newhash[i] == oldhash[i]))
+                    {
+                        i += 1;
+                    }
+                    if (i == newhash.Length)
+                    {
+                        lblshow_correctcode.Text = "secrete value is correct and secure";
+                        lblshow_correctcode.ForeColor = System.Drawing.Color.Green;
+                        btnlogin.Visible = true;
+                    }
+                    else
+                    {
+                        lblshow_correctcode.Text = "Value is not correct!";
+                        lblshow_correctcode.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+                id_shownounceandmessage.Visible = true;
+            }
+            catch(Exception x){}
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            div_usrnamePass.Visible = true;
+        }
+
+        protected void btn_savepass_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtusername.Text.Length >= 6 && txtpassword.Text.Length >= 6)
+                {
+                    string KxorSeed = GetXOR(txtpassword.Text, lblSeed.Text);
+
+                    Int64 N = Convert.ToInt64(lbl_clientN.Text);
+                    for (int i = 0; i < N; i++)
+                    {
+                        KxorSeed = GetHash(KxorSeed, lblSeed.Text);
+
+                    }
+                    string PXorNounce = GetXOR(KxorSeed, lblshownounce_fruser.Text);
+                    //////////save in database serverside:
+                    Database_conn(PXorNounce, Session["N"].ToString(), Session["seedXORnounce"].ToString(), Session["seed"].ToString());
+                    div_loginuser.Visible = true;
+                    div_registerdata.Visible = false;
+                }
+                else
+                {
+                    lblshow_register_errormsg.Text = "User name and password length must be more then 6 characters";
+                    lblshow_register_errormsg.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            catch( Exception x){}
+        }
+        protected void Database_conn( string hashdpass, string N, string nounce, string seed)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=Password_sc;Integrated Security=True");
+                if (cn.State.ToString() == "Closed")
+                {
+                    cn.Open();
+                }
+                SqlCommand cmd1 = new SqlCommand("SELECT UserName FROM tble_oneTimepass WHERE UserName='" + txtusername.Text+ "'", cn);
+                SqlDataReader usernameRdr = null;
+                string user = null;
+                usernameRdr = cmd1.ExecuteReader();
+                while (usernameRdr.Read())
+                {
+                    user = usernameRdr["UserName"].ToString();
+                }
+                cn.Close();
+                if (user == null)
+                {
+                    //hashdpass = GetXOR(hashdpass, lblshownounce_fruser.Text);
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO tble_oneTimepass  values('" + txtusername.Text + "','" + hashdpass + "','" + N + "','" + nounce + "','" + seed + "','" + Session["hD"].ToString() + "','"+ Convert.ToInt32( N)+"')", cn);
+                     cmd.ExecuteNonQuery();
+                     cn.Close();
+                     Response.Write("<script>alert('Register successful');</script>");        
+                }
+                else
+                {
+                    Response.Write("<script>alert('user name already exist!');</script>");
+                }
+            }
+            catch (Exception xe)
+            {
+                //  MessageBox.Show(xe.Message);
+            }
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            div_loginuser.Visible = true;
+            div_registerdata.Visible = false;
+        }
+        protected void getuser_Data() 
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=Password_sc;Integrated Security=True");
+                if (cn.State.ToString() == "Closed")
+                {
+                    cn.Open();
+                }
+                SqlCommand cmd1 = new SqlCommand("SELECT UserId,UserName,P0,Nounce,N, Count,HashD FROM tble_oneTimepass WHERE UserName='" + txtlogin_username.Text + "'", cn);
+                SqlDataReader usernameRdr = null;
+                string user = null;
+                usernameRdr = cmd1.ExecuteReader();
+                while (usernameRdr.Read())
+                {
+                    Session["userid"] = usernameRdr["UserId"].ToString();
+                    Session["user"] = usernameRdr["UserName"].ToString();
+                    Session["pass"] = usernameRdr["P0"].ToString();
+                     Session["seedXORnounce"]= usernameRdr["Nounce"].ToString();
+                     Session["N"] = usernameRdr["N"].ToString();
+                    Session["hD"] = usernameRdr["HashD"].ToString();
+                    Session["count"] = usernameRdr["Count"].ToString();
+                }
+            }
+            catch (Exception xe)
+            {
+                //  MessageBox.Show(xe.Message);
+            }
+ 
+        
+        }
+        protected string check_hashcompare(string oldhash, string newhashnounce) 
+        {
+            if (newhashnounce.Length == oldhash.Length)
+            {
+                int i = 0;
+                while ((i < newhashnounce.Length) && (newhashnounce[i] == oldhash[i]))
+                {
+                    i += 1;
+                }
+                if (i == newhashnounce.Length)
+                {
+                    string retn = "true";
+                    return retn;
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+            else 
+            {
+                return "false";
+            }
+         
+        }
+        protected void btn_login_auth_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                getuser_Data();
+                string Nounce_t = Session["seedXORnounce"].ToString();
+                Nounce_t = GetXOR(Nounce_t, txtlogin_seed.Text);
+                string newhashnounce = GetHash(Nounce_t, txtlogin_seed.Text);
+
+                string oldhash = Session["hD"].ToString();
+                string check = check_hashcompare(oldhash, newhashnounce);
+                if (check == "true")
+                {
+
+                    string KxorSeed = GetXOR(txtlogin_password.Text, txtlogin_seed.Text);
+
+                    Int64 N = Convert.ToInt64(Session["N"].ToString());
+                    for (int i = 0; i < N; i++)
+                    {
+                        KxorSeed = GetHash(KxorSeed, txtlogin_seed.Text);
+
+                    }
+                    string PXorNounce = GetXOR(KxorSeed, Nounce_t);
+                    string checkpass = check_hashcompare(Session["pass"].ToString(), PXorNounce);
+                    if (checkpass == "true")
+                    {
+                        lbllogin_messageshow.Text = "password is correct";  ///////////////call new password update db
+                        ////////////////////////////////////////////////////////    again hash function n time and update previous passowrd:
+                        KxorSeed = GetXOR(txtlogin_password.Text, txtlogin_seed.Text);
+                        N = Convert.ToInt64(Session["N"].ToString());
+                        for (int i = 0; i < N; i++)
+                        {
+                            KxorSeed = GetHash(KxorSeed, txtlogin_seed.Text);
+
+                        }
+                        string newnounce = getNounce().ToString();
+                        PXorNounce = GetXOR(KxorSeed, newnounce);
+                        string newseedXORnounce = GetXOR(newnounce, txtlogin_seed.Text);
+                        ///new nounce and password
+                        ///get hash password
+                        newnounce = GetHash(newnounce, txtlogin_seed.Text); //new hashnounce value for next use
+                        Session["hD"] = newnounce;
+                        int count = Convert.ToInt32(Session["count"].ToString());
+                        if (count != 0)
+                        {
+                            count = count - 1;
+                            update_Data_newpass(PXorNounce, Convert.ToInt32(Session["userid"].ToString()), newseedXORnounce, count, Session["hD"].ToString());
+                        }
+                        else
+                        {
+                            lbllogin_messageshow.Text = "login time expire register again!";
+                            lbllogin_messageshow.ForeColor = System.Drawing.Color.Red;
+                        }
+                    }
+                }
+                else
+                {
+                    lbllogin_messageshow.Text = "Value is not correct!";
+                    lbllogin_messageshow.ForeColor = System.Drawing.Color.Red;
+                }
+            }catch(Exception x){}
+        }
+        protected void update_Data_newpass( string hashpass, int uid,  string nounce, int count, string hashvaluenounce)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=Password_sc;Integrated Security=True");
+                if (cn.State.ToString() == "Closed")
+                {
+                    cn.Open();
+                }
+                SqlCommand cmd1 = new SqlCommand("update tble_oneTimepass set P0='"+hashpass+"',Nounce='"+nounce+"',count='"+count+"', HashD='"+hashvaluenounce+"' where UserId='" + Convert.ToInt32( Session["userid"])+ "'", cn);
+                cmd1.ExecuteNonQuery();
+                cn.Close();
+                Response.Write("<script>alert('authenticate successfully');</script>");
+                Response.Redirect("userprofile.aspx");
+            }
+            catch (Exception xe)
+            {
+                //  MessageBox.Show(xe.Message);
+            }
+
+
+        }
+
+
+    }
+}
